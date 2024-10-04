fae4cdb55254fa9e9883ff274d455592f4b9051a
jpbacher <bacherjp@gmail.com>
2024-10-03 20:03:52 -0400

    added file

--- 0.0-inspect-api-data.py
+++ 0.0-inspect-api-data.py
@@ -7,6 +7,7 @@ config.read_file(open('keys.cfg'))
 API_KEY = config.get("openweathermap", "API_KEY")
 CITY = "Miami"
 
+
 def get_weather_data(api_key, city):
 
     url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=imperial"
@@ -18,9 +19,12 @@ def get_weather_data(api_key, city):
     else:
         raise Exception(f"Error retrieving data from API: {response.status_code}")
     
+
 try:
     weather_data = get_weather_data(api_key=API_KEY, city=CITY)
     print("Weather data fetched successfully!")
     print(weather_data)  
 except Exception as e:
     print(e)
+
+
