0d2ed8f5a13a3a255738b16771b80c910e0b32b6
jpbacher <bacherjp@gmail.com>
2024-10-03 20:13:19 -0400

    added func

--- 1.0-get-fact-dim-data.py
+++ 1.0-get-fact-dim-data.py
@@ -1,2 +1,29 @@
 import configparser
+import datetime
 import requests
+
+
+config = configparser.ConfigParser()
+config.read_file(open('keys.cfg'))
+
+API_KEY = config.get("openweathermap", "API_KEY")
+CITY = "Miami"
+
+
+def get_weather_data(api_key, city):
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=imperial"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data 
+    else:
+        raise Exception(f"Error retrieving data from API: {response.status_code}")
+    
+
+def get_fact_dim_data(data):
+    weather_fact = {
+        'temperature': data['main']['temp'],
+        
+    }
